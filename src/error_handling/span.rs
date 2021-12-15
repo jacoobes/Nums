dfc5cc69af5065f44ce88cf7f4f0cd208fb82028
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2021-12-15 11:26:47 -0600

    redo span fmt, now it looks better

--- src/error_handling/span.rs
+++ src/error_handling/span.rs
@@ -1,38 +1,35 @@
-use super::faults::Faults;
-use ansi_term::{Colour::*, Style};
-use smol_str::SmolStr;
+
 use std::fmt::Debug;
+use smol_str::SmolStr;
+use ansi_term::{Colour::*, Style};
+use super::faults::Faults;
 pub struct Span {
-    //todo : for the file thats being parsed, detect its file path
-    filename: SmolStr,
-    source: String,
-    line: usize,
-    typ: Faults,
+   //todo : for the file thats being parsed, detect its file path 
+   filename : SmolStr,
+   source : String,
+   line: usize,
+   typ:  Faults
 }
 
 impl Debug for Span {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(f, "{: >10?}    ", self.typ)?;
-        writeln!(
-            f,
-            "||  {}  {: >7}",
-            Style::new().bold().paint("@"),
-            Cyan.bold()
-                .paint(format!("{}{}", "src\\main.nums:", self.line))
-        )?;
+        writeln!(f, "||  {}  {: >7}", Style::new().bold().paint("@"), Cyan.bold().paint(format!("{}{}", "src\\main.nums:", self.line)))?;
         writeln!(f, "||")?;
-        writeln!(f, "||  \"{}\"", Style::new().bold().paint(&self.source))?;
-        writeln!(f, "||    {}", "^".repeat(self.source.len()))
+        writeln!(f, "||  \"{}\"", Style::new().bold().paint(&self.source))?;    
+        writeln!(f, "||   {}", "^".repeat(self.source.len()))
     }
 }
 
 impl Span {
-    pub fn new(filename: String, source: String, line: usize, typ: Faults) -> Self {
+    pub fn new(filename: String, source: String,  line: usize,  typ: Faults) -> Self {
         Self {
             filename: SmolStr::from(filename),
             source,
             line,
-            typ,
+            typ
         }
     }
+
+    
 }
