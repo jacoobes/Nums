25b8df0b8086034e01f77e1c2aac5290d001a07c
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2022-02-26 17:27:41 -0600

    Finishing display fmt for tree nodes

--- src/frontend/nodes/stmt.rs
+++ src/frontend/nodes/stmt.rs
@@ -28,15 +28,20 @@ impl std::fmt::Debug for Stmt {
 
     fn fmt(&self, f : &mut std::fmt::Formatter) -> std::fmt::Result {
         match &self {
-            Stmt::Mut(n,e) => write!(f, "[ mut {:?} {:?} ]", n,e),
-            Stmt::Let(n,e) => write!(f, "[ let {:?} {:?} ]", n,e),
-            Stmt::Return(e)=> write!(f, "[ return {:?} ]", e),
+            Stmt::Mut(n,e) => write!(f, "[ MUT {:?} {:?} ]", n,e),
+            Stmt::Let(n,e) => write!(f, "[ LET {:?} {:?} ]", n,e),
+            Stmt::Return(e)=> write!(f, "[ RETURN {:?} ]", e),
+            Stmt::ExprStatement(ex) => write!(f, "{:?}", ex ), 
             Stmt::Block(li) => {
-                write!(f, " block {:?}", li)
+                write!(f, " BLOCK {:?}", li)
             },
-            _other => {
-                write!(f, "unimplemented") 
+            Stmt::IfElse(ex, if_block, else_block) => {
+                write!(f, " [ IF condition : [{:?}] THEN : [ {:?} ], ELSE : [ {:?} ]", ex, if_block, else_block  ) 
+            }
+            Stmt::While(ex, blok) => {
+                write!(f, " [ WHILE condition : [{:?}], THEN [ {:?} ]", ex, blok)
             }
 
-    }
-    } }
+      }
+    } 
+}
